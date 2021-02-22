39a7bb1d3127b51ad60c8794d4fd67384b4f26b7
unknown <mrrandyperrone@gmail.com>
2021-02-22 10:42:10 -0800

    Changed currency to just have dollars and not cents

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -18,6 +18,7 @@ let saveData = (function () {
 let formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
+    maximumFractionDigits: 0
 
     // These options are needed to round to whole numbers if that's what you want.
     //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
